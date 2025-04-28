c706b67200c3d42c7d2668e3d53feecc49f627f0
dserver <dservsolutions@gmail.com>
2025-04-28 12:17:23 -0400

    Project Config

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from tkinter import *
 import tkinter as tk
 
+# Windows Configuration
 window = tk.Tk()
 window.title("Writing Text App")
+
+# Setting the windows size to 500x300
+window.geometry("800x600")
+
+
 
 if __name__ == '__main__':
     window.mainloop()
